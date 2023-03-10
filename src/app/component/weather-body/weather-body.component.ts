02f317c2ffc624d001d0060d83a3da192be76d3d
mofirojean <mofirojean@gmail.com>
2023-03-10 19:47:11 +0100

    feat: added the weather logic

--- src/app/component/weather-body/weather-body.component.ts
+++ src/app/component/weather-body/weather-body.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherData } from 'src/app/models/weather';
+import { WeatherService } from 'src/app/service/weather.service';
 
 @Component({
   selector: 'app-weather-body',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-body.component.scss']
 })
 export class WeatherBodyComponent implements OnInit {
+  cityName: string = "London";
+  weatherData?: WeatherData; 
 
-  constructor() { }
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = ""
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = ""
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeather(cityName)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          this.weatherData = response;
+        }
+      }
+    );
   }
 
 }
