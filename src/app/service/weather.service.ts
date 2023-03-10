12d668a35543c635ab7c6bf773329f30f2599a56
mofirojean <mofirojean@gmail.com>
2023-03-10 19:48:18 +0100

    implemented the weather service

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { WeatherData } from '../models/weather';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  private readonly apiKey: string = 'e18efc0f71be600d1acdeb616832a41a'
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(cityName: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(environment.weatherBaseUrl, {
+      params: new HttpParams()
+        .set('q', cityName)
+        .set('appid', this.apiKey)
+    })
+  }
 }
